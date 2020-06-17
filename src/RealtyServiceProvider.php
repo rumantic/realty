37e1772bedef3d29837834864f629b67c690ec96
Kondin Dmitriy <dkondin@gmail.com>
2020-06-17 18:11:15 +0700

    Sitebill\Realty initial commit

---
+++ src/RealtyServiceProvider.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Sitebill\Realty;
+
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Routing\Router;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class RealtyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        //$this->app->make('Sitebill\Sitebill');
+        //$this->app->register('Sitebill\CRUD');
+
+        /*
+        $this->app->make('Sitebill\Http\Controllers\SitebillGridController');
+        $this->app->register('Sitebill\Http\Controllers\SitebillGridController');
+
+
+        $this->app->register(VoyagerEventServiceProvider::class);
+        $this->app->register(ImageServiceProvider::class);
+        $this->app->register(VoyagerDummyServiceProvider::class);
+        $this->app->register(VoyagerHooksServiceProvider::class);
+        $this->app->register(DoctrineSupportServiceProvider::class);
+
+        $loader = AliasLoader::getInstance();
+        $loader->alias('Voyager', VoyagerFacade::class);
+
+        $this->app->singleton('voyager', function () {
+            return new Voyager();
+        });
+
+        $this->app->singleton('VoyagerGuard', function () {
+            return config('auth.defaults.guard', 'web');
+        });
+
+        $this->loadHelpers();
+
+        $this->registerAlertComponents();
+        $this->registerFormFields();
+
+        $this->registerConfigs();
+
+        if ($this->app->runningInConsole()) {
+            $this->registerPublishableResources();
+            $this->registerConsoleCommands();
+        }
+
+        if (!$this->app->runningInConsole() || config('app.env') == 'testing') {
+            $this->registerAppCommands();
+        }
+        */
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @param \Illuminate\Routing\Router $router
+     */
+    public function boot(Router $router, Dispatcher $event)
+    {
+        //$this->loadViewsFrom(__DIR__.'/../resources/views', 'sitebill');
+        //$this->loadRoutesFrom(__DIR__.'/../routes/routes.php');
+    }
+
+}
