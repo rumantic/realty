f18d21186c80d764581e914c6158be9805661138
Kondin Dmitriy <dkondin@gmail.com>
2020-06-30 19:45:47 +0700

    шаблон layouts

--- src/resources/views/home/index.blade.php
+++ src/resources/views/home/index.blade.php
@@ -1,1 +1,11 @@
-realty index
+@extends('sitebill_realty::layouts.default')
+
+
+
+
+@section('content')
+@endsection
+
+@section('after_content_widgets')
+    @include(backpack_view('inc.widgets'), [ 'widgets' => app('widgets')->where('section', 'after_content')->toArray() ])
+@endsection
